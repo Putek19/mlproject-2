0312dc6fd23560db98787cd58158c7813691ed27
Putek19 <kubanowacki.jn@gmail.com>
2025-02-08 18:51:20 +0100

    ETL Pipeline

---
+++ push_data.py
@@ -0,0 +1,63 @@
+import os
+import sys
+import json
+from dotenv import load_dotenv
+
+load_dotenv()
+
+MONGO_DB_URL = os.getenv("MONGO_DB_URL")
+
+
+import certifi
+
+ca = certifi.where()
+
+import pandas as pd
+import numpy as np
+import pymongo
+from project.exception.exception import ProjectException
+from project.logging.logger import logging
+
+class ChurnDataExtract():
+    def __init__(self):
+        try:
+            pass
+        except Exception as e:
+            raise ProjectException(e,sys)
+    
+    def csv_to_json_convertor(self,file_path):
+        try:
+            data = pd.read_csv(file_path)
+            data.reset_index(drop=True,inplace=True)
+            records = list(json.loads(data.T.to_json()).values())
+            return records
+        except Exception as e:
+            raise ProjectException(e,sys)
+    def insert_data_to_mongodb(self,records,database,collection):
+        try:
+            self.database = database
+            self.collection = collection
+            self.records = records
+
+            self.mongo_client = pymongo.MongoClient(MONGO_DB_URL)
+            self.database = self.mongo_client[self.database]
+
+            self.collection = self.database[self.collection]
+            self.collection.insert_many(self.records)
+            return (len(self.records))
+
+        except Exception as e:
+            raise ProjectException(e,sys)
+        
+if __name__ == "__main__":
+    FILE_PATH = "Data\churn_data.csv"
+    DATABASE = "KUBAN"
+    Collection = "ChurnData"
+    churnobj = ChurnDataExtract()
+    records = churnobj.csv_to_json_convertor(FILE_PATH)
+    print(records)
+    noofrecords = churnobj.insert_data_to_mongodb(records,DATABASE,Collection )
+    print(noofrecords)
+
+
+
