cb2ff02bdff856ff1962c948ee37019c49505094
Putek19 <kubanowacki.jn@gmail.com>
2025-02-14 21:53:57 +0100

    mlflow experiments

--- project/components/model_trainer.py
+++ project/components/model_trainer.py
@@ -1,4 +1,6 @@
 import os,sys
+from urllib.parse import urlparse
+import mlflow
 import numpy as np
 
 from project.exception.exception import ProjectException
@@ -28,6 +30,21 @@ class ModelTrainer:
             self.data_transformation_artifact = data_transformation_artifact
         except Exception as e:
             raise ProjectException(e, sys)
+        
+    def track_mlflow(self,best_model,classificationmetric):
+        with mlflow.start_run():
+            f1_score=classificationmetric.f1_score
+            precision_score=classificationmetric.precision_score
+            recall_score=classificationmetric.recall_score
+
+            
+
+            mlflow.log_metric("f1_score",f1_score)
+            mlflow.log_metric("precision",precision_score)
+            mlflow.log_metric("recall_score",recall_score)
+            mlflow.sklearn.log_model(best_model,"model")
+        
+
     
     def train_model(self, X_train, y_train,X_test,y_test):
         try:
@@ -61,7 +78,10 @@ class ModelTrainer:
             y_test_pred = best_model.predict(X_test)
 
             classification_train_metric = get_classification_metrics(y_true=y_train,y_pred=y_train_pred)
+            self.track_mlflow(best_model,classification_train_metric)
+
             classification_test_metric = get_classification_metrics(y_true=y_test,y_pred=y_test_pred)
+            self.track_mlflow(best_model,classification_test_metric)
 
             preprocessor = load_object(self.data_transformation_artifact.transformed_object_file_path)
 
